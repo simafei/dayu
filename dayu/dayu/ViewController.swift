d91b2d2105cd608f634eaf4654dda43b8d659bb8
simafei <xinger@XingerdeMac-mini.local>
2015-08-27 23:23:06 +0800

    init import

---
+++ dayu/dayu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dayu
+//
+//  Created by Xinger on 15/8/27.
+//  Copyright (c) 2015年 Xinger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
